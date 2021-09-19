2798f411e2e4e4602ca95fd799cb7877e5b21aa2
Hisashi <hisasi622@gmail.com>
2021-09-19 10:43:57 +0900

    jsonからデータを取得

---
+++ pages/search.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .page-search
+    h1 ユーザー情報一覧
+    button(ref="btn") 情報取得
+    .user-list
+      ul
+        li(v-for="user in users" :key="user.id")
+          p {{ user.name }}
+          p {{user.username}}
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  mounted() {
+    this.$refs.btn.addEventListener('click',  this.getUsers)
+  },
+  methods : {
+    getUsers() {
+      axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => (this.users = response.data))
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+.page-search
+  .user-list
+    ul
+      display flex
+      justify-content space-between
+      flex-wrap wrap
+      li
+        margin-top 15px
+        width 25%
+</style>

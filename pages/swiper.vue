f26eb9882b59e04341ab9e2cb626137041ca0e27
Hisashi <hisasi622@gmail.com>
2021-09-20 12:13:16 +0900

    スワイパーのデモ追加

---
+++ pages/swiper.vue
@@ -0,0 +1,40 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <div slot="pagination" class="swiper-pagination"></div>
+    <div slot="button-prev" class="swiper-button-prev"></div>
+    <div slot="button-next" class="swiper-button-next"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        slidesPerView: 1,
+        loop: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+.swiper-button-prev
+  left 0
+</style>

4ef31f596a55278539df6cca27ff57ede1f6644a
Hisashi <hisasi622@gmail.com>
2021-09-26 14:11:03 +0900

    h1のデフォルトスタイル調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,4 +14,11 @@ div
   .nuxt
     max-width 1280px
     margin 0 auto
+h1
+  border-left 3px solid main-color
+  padding-left 15px
+  box-sizing border-box
+  font-size rem(24px)
+  line-height rem(32px)
+  margin-bottom 30px
 </style>

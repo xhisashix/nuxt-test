23eb92818624e451f8205b24c60f108b267dc173
Hisashi <hisasi622@gmail.com>
2021-09-26 13:52:23 +0900

    データの使い方

---
+++ pages/data.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.page-data
+  p {{ text }}
+  ul
+    li
+      p {{ lists[0].title }}
+      p {{ lists[0].content }}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "データの出力",
+            lists: [{
+                title: "タイトル",
+                content: "コンテンツ",
+            }]
+        }
+    },
+}
+</script>
+
+<style lang="stylus">
+
+</style>

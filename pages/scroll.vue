b4343ada5de433581eee4ee75187e84d72436bb1
Hisashi <hisasi622@gmail.com>
2021-10-09 21:46:30 +0900

    3秒後のクラスを追加できるように

--- pages/scroll.vue
+++ pages/scroll.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div.scroll
   .scroll-position スクロール位置の取得 : {{ scrollY }}
-  .section(ref="section1")
+  .section(ref="section1" :class="{now : now }")
     p section1の高さ：{{section1}}
   .section(ref="section2")
     p section2の高さ：{{section2}}
@@ -9,26 +9,35 @@ div.scroll
 
 <script>
 export default {
-    data() {
-        return {
-            scrollY: 0,
-            section1: "",
-            section2: "",
-        }
-    },
-    mounted() {
-        window.addEventListener("scroll", this.getScroll)
-        this.getHeight()
-    },
-    methods: {
-        getScroll() {
-            this.scrollY = window.scrollY
-        },
-        getHeight() {
-            this.section1 = this.$refs.section1.offsetTop;
-            this.section2 = this.$refs.section2.offsetTop;
-        }
+  data() {
+    return {
+      scrollY: 0,
+      section1: '',
+      now: false,
+      section2: '',
     }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.getScroll)
+    this.getHeight()
+  },
+  methods: {
+    getScroll() {
+      this.scrollY = window.scrollY
+      this.addClass()
+    },
+    getHeight() {
+      this.section1 = this.$refs.section1.offsetTop
+      this.section2 = this.$refs.section2.offsetTop
+    },
+    addClass() {
+      if (this.scrollY > this.section1) {
+        setTimeout(() => {
+          this.now = true
+        }, 3000)
+      }
+    },
+  },
 }
 </script>
 

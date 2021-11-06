242ad82ae3dcaded89989aba808c0e1b464f7a58
Hisashi <hisasi622@gmail.com>
2021-11-06 23:48:18 +0900

    swiperのボタンが動くように

--- components/ui/VueSwiper.vue
+++ components/ui/VueSwiper.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-.main-swiper
-  client-only
-    swiper(:options="swiperOption")
-      swiper-slide
-        img(src="/image/main/main1.jpg", alt="alt")
-      swiper-slide
-        img(src="/image/main/main2.jpg", alt="alt")
-      swiper-slide
-        img(src="/image/main/main3.jpg", alt="alt")
-      div(slot="pagination" class="swiper-pagination")
-      div(slot="button-prev" class="swiper-button-prev")
-      div(slot="button-next" class="swiper-button-next")
+  .main-swiper
+    client-only
+      swiper(:options="swiperOption")
+        swiper-slide
+          img(src="/image/main/main1.jpg", alt="alt")
+        swiper-slide
+          img(src="/image/main/main2.jpg", alt="alt")
+        swiper-slide
+          img(src="/image/main/main3.jpg", alt="alt")
+    .swiper-pagination(slot="pagination")
+    .swiper-button-prev(slot="button-prev")
+    .swiper-button-next(slot="button-next")
+    .swiper-scrollbar(slot="swiper-scrollbar")
 </template>
 
 <script>
@@ -22,16 +23,23 @@ export default {
           delay: 5000,
           disableOnInteraction: false,
         },
-        slidesPerView: 1,
+        slidesPerView: 1.5,
         loop: true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        effect: 'cards',
+        speed: 1000,
+        spaceBetween: 40,
+        centeredSlides: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
       },
     }
   },
@@ -40,8 +48,17 @@ export default {
 
 <style lang="stylus">
 .main-swiper
+  position relative
+  padding 20px 0
   img
     width 100%
     height auto
     max-height 600px
+  .swiper-pagination
+    position: absolute
+    bottom 0
+    top auto
+    left 0
+    right 0
+    margin auto
 </style>

b1a0bba266970493c87a4620b0d1ba484e5835fb
Hisashi <hisasi622@gmail.com>
2021-12-07 22:26:16 +0900

    フォームの大枠追加

---
+++ pages/demo/check.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .form-check
+    form(action='cgi-bin/formmail.cgi' method='post')
+      p
+        | 名前：
+        input(type='text' name='name' size='40')
+      p
+        | 性別：
+        input(type='radio' name='sex' value='male')
+        | 男
+        input(type='radio' name='sex' value='female')
+        | 女
+      p
+        | 血液型：
+        select(name='blood')
+          option(value='A') A型
+          option(value='B') B型
+          option(value='O') O型
+          option(value='AB') AB型
+      p
+        | ご感想：
+        br
+        textarea(name='kanso' rows='4' cols='40')
+      p
+        input(type='submit' value='送信')
+        input(type='reset' value='リセット')
+
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus">
+.form-check
+  form
+    tr
+      margin-top: 20px
+      td
+        margin-top: 20px
+</style>

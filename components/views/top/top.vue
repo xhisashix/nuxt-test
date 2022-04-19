1de6e5d972dfd50d1a313c892989cdd82bdcd330
Hisashi <hisasi622@gmail.com>
2022-04-19 22:05:50 +0900

    TOP追加

---
+++ components/views/top/top.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.top-main
+  .main-mv
+    img(src="image/top/main.png" alt="altText")
+    .main-text
+      p eLogs
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus">
+.top-main
+  .main-mv
+    position: relative
+    img
+      width: 100%
+      height: auto
+    .main-text
+      position: absolute
+      display: flex
+      justify-content: center
+      align-items: center
+      top 0
+      left 0
+      width: 100%
+      height: 100%
+    p
+      font-size rem(128px)
+      font-family: 'Pinyon Script', cursive;
+      font-weight bold
+</style>
---
+++ pages/demo/computed.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .page-computed
+      p FirstName: {{ firstName }}
+      p LastName: {{ lastName }}
+      p FullName: {{ fullName }}
+      .multiplication
+        p {{ a }}
+        p {{ b }}
+        p {{ multiplication }}
+</template>
+<script>
+export default {
+  data() {
+    return {
+      firstName: 'Taro',
+      lastName: 'Tanaka',
+      a: 10,
+      b: 6,
+    }
+  },
+  computed: {
+    fullName() {
+      return this.lastName + this.firstName
+    },
+    multiplication() {
+      const answer = this.a * this.b
+      if(answer <= 50){
+        return answer
+      } else {
+        return "50以上です。"
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.page-computed
+  padding-top 80px
+  p
+    font-size rem(20px)
+    line-height rem(28px)
+</style>

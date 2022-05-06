ac240509f2ab80693dd45c254e47b4d1d8654385
Hisashi <hisasi622@gmail.com>
2022-05-07 00:19:15 +0900

    文字の表示アニメーション

--- components/views/top/top.vue
+++ components/views/top/top.vue
@@ -3,10 +3,32 @@
   .main-mv
     img(src="image/top/main.png" alt="altText")
     .main-text
-      p eLogs
+      p(:class="{ act: isAnim }")
+        span e
+        span L
+        span o
+        span g
+        span s
 </template>
 <script>
-export default {}
+import Scrollmagic from '~/components/ui/Scrollmagic'
+export default {
+  components: {
+    Scrollmagic
+  },
+  data() {
+    return {
+      isAnim: false
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.isAnim = true
+      }, 1000)
+    })
+  }
+}
 </script>
 <style lang="stylus">
 .top-main
@@ -30,4 +52,21 @@ export default {}
       font-weight bold
       +bp(md)
         font-size rem(72px)
+      &.act
+        span
+          opacity 1
+          transform: translateX(0)
+          &:nth-child(2)
+            transition-delay: .3s
+          &:nth-child(3)
+            transition-delay: .5s
+          &:nth-child(4)
+            transition-delay: .7s
+          &:nth-child(5)
+            transition-delay: .9s
+      span
+        display: inline-block
+        opacity: 0
+        transition: .7s
+        transform: translateX(10px) rotate(-30deg)
 </style>
